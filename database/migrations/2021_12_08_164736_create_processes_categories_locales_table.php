4b7308cc3510a524c177a264f464245e875a8c3f
Kamran Garavaliyev <garavaliyevkamran@gmail.com>
2022-01-21 13:52:25 +0400

    migration problem

--- database/migrations/2021_12_08_164736_create_processes_categories_locales_table.php
+++ database/migrations/2021_12_08_164736_create_processes_categories_locales_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProcessCategoriesLocalesTable extends Migration
+class CreateProcessesCategoriesLocalesTable extends Migration
 {
     /**
      * Run the migrations.
