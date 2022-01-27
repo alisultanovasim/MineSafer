ae2be0fb38ec8819ab6acfa283b90c42e06b48d2
Kamran Garavaliyev <garavaliyevkamran@gmail.com>
2022-01-27 13:42:31 +0400

    bug fixed

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -28,9 +28,9 @@ class PhotoController extends Controller
     public function show($id)
     {
         if (auth()->check()) {
-            $photoFolder = Photo::with('images', 'folders')->where('id', $id)->first();
+            $photoFolder = Photo::with('images', 'folders')->first($id);
         } else {
-            $photoFolder = Photo::with('images', 'folder')->where('id', $id)->first();
+            $photoFolder = Photo::with('images', 'folder')->first($id);
         }
         return $this->dataResponse($photoFolder);
     }
